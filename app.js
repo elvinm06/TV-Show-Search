27ac087ffdb06eaa73a1b47f013a181d52891152
elvinm06 <141499967+elvinm06@users.noreply.github.com>
2024-03-21 14:23:18 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const form = document.querySelector("#searchForm");
+
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+  try {
+      const searchTerm = form.elements.query.value;
+      const config = { params: { q: searchTerm } };
+      const res = await axios.get(`http://api.tvmaze.com/search/shows`, config);
+      console.log(res.data[0].show.image.medium);
+      // window.open(res.data[0].show.image.medium, "_blank");
+      makeImages(res.data);
+      form.elements.query.value = "";
+    
+  } catch (e) {
+    console.log("Error", e);
+  }
+});             
+
+const makeImages = (shows) => {
+  for (let result of shows) {
+    if (result.show.image) {
+      const img = document.createElement("IMG");
+      img.src = result.show.image.medium;
+      document.body.append(img);
+    }
+  }
+};
